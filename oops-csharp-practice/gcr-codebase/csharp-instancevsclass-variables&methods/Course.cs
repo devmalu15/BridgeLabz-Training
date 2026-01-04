000836f543c2a169420e6f34939f23058a291121
Dev Malu <maludev15@gmail.com>
2026-01-04 23:44:08 +0530

    [Dev Malu] Add .Added instance vs class variables and methods folder

---
+++ oops-csharp-practice/gcr-codebase/csharp-instancevsclass-variables&methods/Course.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.InstanceVsClassVariablesAndMethods
+{
+    public class Course
+    {
+        string courseName;
+        int duration;
+        double fee;
+        static string instituteName = "Tech Institute";
+
+        public Course(string courseName, int duration, double fee)
+        {
+            this.courseName = courseName;
+            this.duration = duration;
+            this.fee = fee;
+        }
+
+        public void DisplayCourseDetails()
+        {
+            Console.WriteLine("Institute: " + instituteName);
+            Console.WriteLine("Course: " + courseName);
+        }
+
+        public static void UpdateInstituteName(string newName)
+        {
+            instituteName = newName;
+        }
+
+        public static void Main(string[] args)
+        {
+            Course c1 = new Course("Java", 3, 15000);
+            UpdateInstituteName("Code Academy");
+            c1.DisplayCourseDetails();
+        }
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-instancevsclass-variables&methods/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.InstanceVsClassVariablesAndMethods
+{
+    public class Product
+    {
+        string productName;
+        double price;
+        static int totalProducts = 0;
+
+        public Product(string productName, double price)
+        {
+            this.productName = productName;
+            this.price = price;
+            totalProducts++;
+        }
+
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine("Product: " + productName);
+            Console.WriteLine("Price: " + price);
+        }
+
+        public static void DisplayTotalProducts()
+        {
+            Console.WriteLine("Total Products Created: " + totalProducts);
+        }
+
+        public static void Main(string[] args)
+        {
+            Product p1 = new Product("Laptop", 50000);
+            Product p2 = new Product("Mouse", 500);
+            p1.DisplayProductDetails();
+            DisplayTotalProducts();
+        }
+    }
+}
