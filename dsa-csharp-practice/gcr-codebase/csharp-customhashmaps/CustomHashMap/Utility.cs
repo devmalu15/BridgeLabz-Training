9bda05e52a246cc2585c92ce8c9c80e0e71a4b54
Dev Malu <maludev15@gmail.com>
2026-01-09 11:56:31 +0530

    [Dev Malu] Add .Added custom hashmaps folder

---
+++ dsa-csharp-practice/gcr-codebase/csharp-customhashmaps/CustomHashMap/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.HashMapHashSet.CustomHashMap
+{
+    public class Utility
+    {
+        public static void Main(string[] args)
+        {
+            MyHashMap<string, string> map = new MyHashMap<string, string>();
+            map.Put("Loveeeesh", "Engineer");
+            map.Put("Ramaa", "Doctor");
+            map.Put("Risita", "Artist");
+
+            Console.WriteLine("Loveeeesh is: " + map.Get("Loveeeesh"));
+            Console.WriteLine("Ramaa is: " + map.Get("Ramaa"));
+
+            if (map.ContainsKey("Risita"))
+            {
+                Console.WriteLine("Risita is present.");
+            }
+
+            map.Remove("Ramaa");
+            if (!map.ContainsKey("Ramaa"))
+            {
+                Console.WriteLine("Ramaa removed.");
+            }
+        }
+    }
+}
---
+++ dsa-csharp-practice/gcr-codebase/csharp-customhashmaps/LongestConsecutiveSubsequence/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.HashMapHashSet.LongestConsecutiveSubsequence
+{
+    public class Utility
+    {
+        public static void Main(string[] args)
+        {
+            MyHashMap<string, string> map = new MyHashMap<string, string>();
+            int[] arr = { 100, 4, 200, 1, 3, 2 };
+            int result = map.FindLongest(arr);
+            Console.WriteLine("Longest sequence for Risita is: " + result);
+        }
+    }
+}
