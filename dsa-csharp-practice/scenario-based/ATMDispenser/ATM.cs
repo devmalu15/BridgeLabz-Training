ccf0797db4fba7318fa8ac0f904f6f4d8491baad
Dev Malu <maludev15@gmail.com>
2026-01-09 15:31:50 +0530

    [Dev Malu] Add .Added ATM Dispenser Scenario Based

---
+++ dsa-csharp-practice/scenario-based/ATMDispenser/ATM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.ScenarioBased.ATMDispenser
+{
+    public class ATM
+    {
+        public int balance { get; set; }
+        public int[] denomination { get; set; }
+
+        public ATM(int balance, int[] denomination)
+        {
+            this.balance = balance;
+            this.denomination = denomination;
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/ATMDispenser/IDispensable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.ScenarioBased.ATMDispenser
+{
+    public interface IDispensable
+    {
+        public void Dispense(ATM atm, int amount);
+    }
+}
