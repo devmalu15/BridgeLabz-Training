b4c1b38fcc21628e1a076a6fb8a0937c55e912e0
Dev Malu <maludev15@gmail.com>
2026-01-14 11:03:03 +0530

    [Dev Malu] Add .Added Algorithm Analysis Folder

---
+++ dsa-csharp-practice/gcr-codebase/chsarp-algorithm-analysis/StringConcatComparison.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.AlgorithmAnalysis
+{
+    public class StringConcatComparison
+    {
+        public static void Main(string[] args)
+        {
+            int iterations = 10000;
+            string[] names = { "lovish", "risita", "ramaa" };
+
+            Stopwatch sw = Stopwatch.StartNew();
+            string s = "";
+            for (int i = 0; i < iterations; i++)
+            {
+                s += names[i % 3];
+            }
+            sw.Stop();
+            Console.WriteLine($"String Concatenation Time: {sw.Elapsed.TotalMilliseconds} ms");
+
+            sw.Restart();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < iterations; i++)
+            {
+                sb.Append(names[i % 3]);
+            }
+            sw.Stop();
+            Console.WriteLine($"StringBuilder Time: {sw.Elapsed.TotalMilliseconds} ms");
+        }
+    }
+}
