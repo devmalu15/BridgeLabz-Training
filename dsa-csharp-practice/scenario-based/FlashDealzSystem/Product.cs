144b6738e08f77bc86288792622f06337789c6f6
Dev Malu <maludev15@gmail.com>
2026-01-17 15:22:18 +0530

    [Dev Malu] Add .Added Scenario Based Folders

---
+++ dsa-csharp-practice/scenario-based/FlashDealzSystem/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.ScenarioBased.FlashDealz
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int DiscountPercentage { get; set; }
+
+        public Product(string name, int discount)
+        {
+            Name = name;
+            DiscountPercentage = discount;
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/FlashDealzSystem/RunFlashDealzApp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabsTrainingVS.ScenarioBased.FlashDealzSystem
+{
+    public class RunFlashDealzApp
+    {
+        public static void Main(string[] args)
+        {
+            IFlashManager manager = new FlashManagerImpl();
+            bool running = true;
+
+            Console.WriteLine("=== FLASH DEALZ MANAGER ===");
+
+            while (running)
+            {
+                FlashUtility.ShowMenu();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        string name = FlashUtility.GetString("Product Name");
+                        double disc = FlashUtility.GetDouble("Discount %");
+                        manager.AddProduct(name, disc);
+                        break;
+                    case "2":
+                        manager.SortProducts();
+                        break;
+                    case "3":
+                        manager.DisplayDeals();
+                        break;
+                    case "4":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+        }
+    }
+}
